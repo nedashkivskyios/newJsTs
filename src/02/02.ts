54a1330d6c5c5508528d40e8641a1dfdb8cb00a2
nedashkivskyios <nedashkivskyios@gmail.com>
2021-11-03 14:11:25 +0200

    2

---
+++ src/02/02.ts
@@ -0,0 +1,39 @@
+type LocalCityType = {
+  title: string
+  countryTitle: string
+}
+type AddressType = {
+  streetTitle: string
+  city: LocalCityType
+}
+type TechnologiesType = {
+  id: number
+  title: string
+}
+type StudentType = {
+  id: number
+  name: string
+  age: number
+  isActive: boolean
+  address: AddressType
+  technologies: TechnologiesType[]
+}
+
+export const student: StudentType = {
+  id: 1,
+  name: "Sasha",
+  age: 28,
+  isActive: true,
+  address: {
+    streetTitle: 'Krymskogo',
+    city: {
+      title: "BC",
+      countryTitle: "UA",
+    },
+  },
+  technologies: [
+    {id: 1, title: "HTML"},
+    {id: 2, title: "CSS"},
+    {id: 3, title: "JS"},
+  ],
+}
