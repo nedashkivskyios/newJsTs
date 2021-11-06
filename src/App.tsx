5fd040c25ec74b71b4acc90b143845d2b7696488
nedashkivskyios <nedashkivskyios@gmail.com>
2021-11-06 23:05:56 +0200

    test end commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Tes from "./Test/Test";
 
 function App() {
   return (
     <div className="App">
-      <Tes />
     </div>
   );
 }
